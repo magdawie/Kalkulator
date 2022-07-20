75bcdaae7ad1ff94a87acb6bad0436bf84c759c7
Magdalena Wieczorek <magd.wieczorek@gmail.com>
2022-07-19 23:52:22 -0400

    calc edit

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -1,11 +1,15 @@
 package com.kalkulator;
 
 public class Calculator {
-    public void someMethod(){
-        System.out.println("It works!");
+    public void subtract(){
+        System.out.println("subtract works!");
+    }
+    public void add(){
+        System.out.println("add works!");
     }
     public static void main(String args[]){
         Calculator calculator = new Calculator();
-        calculator.someMethod();
+        calculator.subtract();
+        calculator.add();
     }
 }
